8bca7bfd84775e97106b6152c72f6d52a9b74f74
pablols02 <lspablo02@gmail.com>
2024-02-06 14:15:06 +0100

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -7,19 +7,38 @@ import Nosotros from './componentes/nosotros';
 import Planes from './componentes/planes';
 import Contacto from './componentes/contacto';
 import Pie from './componentes/pie';
+import { useState, useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
 
 function App() {
+  const [loading,setLoading] = useState(false)
+  useEffect(()=>{
+  setLoading(true)
+  setTimeout(()=>{
+  setLoading(false)
+  
+  },3000)
+  
+  },[])
+
   return (
     <div className="App">
-      <div><Cabecera/></div>
-      <div><Inicio/></div>
-      <div><Programa/></div>
-      <div><Reserva/></div>
-      <div><Nosotros/></div>
-      <div><Planes/></div>
-      <div><Contacto/></div>
-      <div><Pie/></div>
+      {
+        loading?
+        <HashLoader color="#d79447" className='loader'/>
+      :
+      <div>
+        <div><Cabecera/></div>
+        <div><Inicio/></div>
+        <div><Programa/></div>
+        <div><Reserva/></div>
+        <div><Nosotros/></div>
+        <div><Planes/></div>
+        <div><Contacto/></div>
+        <div><Pie/></div>
+      </div>
+      }
     </div>
   );
 }
